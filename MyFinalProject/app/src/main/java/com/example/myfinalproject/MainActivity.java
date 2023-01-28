5cc20295c5a9198e5db2bec91ee0c1fb51ce60cd
shanishilko <shanishilko@gmail.com>
2023-01-28 09:13:56 +0200

    first

---
+++ MyFinalProject/app/src/main/java/com/example/myfinalproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings, menu);
+        inflater.inflate(R.menu.menu_exit, menu);
+        return true;
+    }
+}
